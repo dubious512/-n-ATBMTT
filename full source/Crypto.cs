290f93f77749e8c317e6bf16badce81c78363b7b
Nguyễn Cường <cuong2k3@gmail.com>
2023-06-14 12:43:25 +0700

    Add files via upload

---
+++ full source/Crypto.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace ATBMTT
+{
+    internal class Crypto
+    {
+        public static byte[] getEncryptor(string strKey, byte[] textByte, string strSalt)
+        {
+            byte[] keyByte = Encoding.UTF8.GetBytes(strKey);
+            byte[] saltByte = null;
+            if (string.IsNullOrEmpty(strSalt))
+            {
+                saltByte = new byte[] { 0x11, 0x00, 0x56, 0x43, 0x11, 0x77, 0x13, 0x88, 0x30, 0x04, 0x99, 0x54, 0x41, 0x83, 0x69, 0x51 };
+            }
+            else
+            {
+                saltByte = Encoding.UTF8.GetBytes(strSalt);
+            }
+            byte[] encryptedFile = null;
+
+            try
+            {
+                using (var memory = new MemoryStream())
+                {
+                    using (var aes = new RijndaelManaged())
+                    {
+                        aes.Mode = CipherMode.CBC;
+                        aes.Padding = PaddingMode.PKCS7;
+                        aes.KeySize = 256;
+                        aes.BlockSize = 128;
+
+                        var key = new Rfc2898DeriveBytes(keyByte, saltByte, 2000);
+                        aes.Key = key.GetBytes(aes.KeySize / 8);
+                        aes.IV = key.GetBytes(aes.BlockSize / 8);
+
+                        using (var ICrypto = new CryptoStream(memory, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                        {
+                            ICrypto.Write(textByte, 0, textByte.Length);
+                            ICrypto.FlushFinalBlock();
+                            ICrypto.Close();
+
+                        }
+                    }
+                    encryptedFile = memory.ToArray();
+                    memory.Close();
+                }
+                return encryptedFile;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static byte[] getDecryptor(string strKey, byte[] textByte, string strSalt)
+        {
+            byte[] keyByte = Encoding.UTF8.GetBytes(strKey);
+            byte[] saltByte = null;
+            if (string.IsNullOrEmpty(strSalt))
+            {
+                saltByte = new byte[] { 0x11, 0x00, 0x56, 0x43, 0x11, 0x77, 0x13, 0x88, 0x30, 0x04, 0x99, 0x54, 0x41, 0x83, 0x69, 0x51 };
+            }
+            else
+            {
+                saltByte = Encoding.UTF8.GetBytes(strSalt);
+            }
+            byte[] encryptedFile = null;
+
+            try
+            {
+                using (var memory = new MemoryStream())
+                {
+                    using (var aes = new RijndaelManaged())
+                    {
+                        aes.Mode = CipherMode.CBC;
+                        aes.Padding = PaddingMode.PKCS7;
+                        aes.KeySize = 256;
+                        aes.BlockSize = 128;
+
+                        var key = new Rfc2898DeriveBytes(keyByte, saltByte, 2000);
+                        aes.Key = key.GetBytes(aes.KeySize / 8);
+                        aes.IV = key.GetBytes(aes.BlockSize / 8);
+
+                        using (var ICrypto = new CryptoStream(memory, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                        {
+                            ICrypto.Write(textByte, 0, textByte.Length);
+                            ICrypto.FlushFinalBlock();
+                            ICrypto.Close();
+
+                        }
+                    }
+                    encryptedFile = memory.ToArray();
+                    memory.Close();
+                }
+                return encryptedFile;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ full source/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ATBMTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            OpenFileDialog = new OpenFileDialog();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.GradientActiveCaption;
+            label1.Location = new Point(92, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Chọn file";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Gray;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(428, 42);
+            button1.Name = "button1";
+            button1.Size = new Size(30, 23);
+            button1.TabIndex = 1;
+            button1.Text = "...";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = SystemColors.ButtonHighlight;
+            textBox1.Location = new Point(153, 42);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(269, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // OpenFileDialog
+            // 
+            OpenFileDialog.FileName = "openFileDialog1";
+            OpenFileDialog.RestoreDirectory = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(92, 89);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Mật khẩu";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(155, 86);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(133, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightCoral;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(186, 142);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 5;
+            button2.Text = "Mã hóa";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.LimeGreen;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Location = new Point(318, 142);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "Giải mã";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientActiveCaption;
+            ClientSize = new Size(606, 223);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+        private OpenFileDialog OpenFileDialog;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button2;
+        private Button button3;
+    }
+}
